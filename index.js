7be81750959654375cf41b1c777b5792483f804c
MazeQhi404 <maze.putuma@gmail.com>
2024-11-19 00:27:43 +0200

    created a startGame function, renderGame function that sums up the card numbers, ,and a getRandomNumber that generates random numbers between 1 and 13.

--- index.js
+++ index.js
@@ -3,21 +3,46 @@
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 10
-let secondCard = 11
 
-let sum = firstCard + secondCard
+let cards = []
+
+let sum = 0
 
 let hasBlackJack = false
-let isAlive= true
+let isAlive= false
 let message= ""
 
 let messageEl = document.getElementById("message-el")
 let sumEl= document.getElementById("sum-el")
 let cardsEl= document.getElementById("cards-el")
 
+function getRandomCard () {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if ( randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1 ) {
+        return 11
+    } else {
+        return randomNumber
+    }
+
+}
+
 function startGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard 
+    let firstCard = getRandomCard ()
+    let secondCard = getRandomCard ()
+    cards= [firstCard, secondCard]
+    sum = firstCrad + secondCard
+
+    renderGame()
+}
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+
+    for (let i = 0; i < cards.length; i++) {
+
+       cardsEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = "Sum: " + sum
 
     if (sum <= 20) {
@@ -38,6 +63,16 @@ function startGame() {
 
 function newCard() {
 console.log("Drawing a new card from the deck!")
+
+let cardDrawn= getRandomCard ()
+
+sum += cardDrawn
+
+cards.push(cardDrawn)
+console.log(cards)
+
+renderGame ()
+
 }
 
 
