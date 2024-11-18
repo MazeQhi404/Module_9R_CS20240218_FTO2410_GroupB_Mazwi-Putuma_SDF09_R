b14c59da8895f00f3757d65083ee0599b37065ed
MazeQhi404 <maze.putuma@gmail.com>
2024-11-18 17:42:25 +0200

    created and styled a start button; removed display layout and aligned the text in the centre instead.

--- index.js
+++ index.js
@@ -12,19 +12,27 @@ let hasBlackJack = false
 let isAlive= true
 let message= ""
 
-if (sum <= 20) {
+function startGame() {
+    if (sum <= 20) {
 
-    message = "Do you want to draw a new card?"
+        message = "Do you want to draw a new card?"
+    
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    
+    } else {
+        message= "You're out of the game!" 
+        isAlive=false
+    }
+    
+    console.log(message)
+    console.log(hasBlackJack)
+    console.log(isAlive)
 
-} else if (sum === 21) {
-    message = "You've got Blackjack!"
-    hasBlackJack = true
-
-} else {
-    message= "You're out of the game!" 
-    isAlive=false
 }
 
 console.log(message)
 console.log(hasBlackJack)
 console.log(isAlive)
+
