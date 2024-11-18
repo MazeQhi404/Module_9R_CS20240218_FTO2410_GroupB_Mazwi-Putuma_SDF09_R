d184954f94aba6cd4b990624721abcf2e7208355
MazeQhi404 <maze.putuma@gmail.com>
2024-11-18 14:47:46 +0200

    Used conditional statements to output messages based on the sum of the card variables

--- index.js
+++ index.js
@@ -3,9 +3,28 @@
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 6
-let secondCard = 9
+let firstCard = 10
+let secondCard = 12
 
 let sum = firstCard + secondCard
 
-console.log(sum)
+let hasBlackJack = false
+let isAlive= true
+let message= ""
+
+if (sum <= 20) {
+
+    message = "Do you want to draw a new card?"
+
+} else if (sum === 21) {
+    message = "You've got Blackjack!"
+    hasBlackJack = true
+
+} else {
+    message= "You're out of the game!" 
+    isAlive=false
+}
+
+console.log(message)
+console.log(hasBlackJack)
+console.log(isAlive)
